ffe0d0c050226480c8deb5661b6010d32bc9cfbf
Mark Fuechec <fuechec.mark@gmail.com>
2020-02-07 16:08:32 -0600

    Implemented react native router

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { NativeRouter, Route } from "react-router-native";
 import LandingPage from './components/LandingPage/LandingPage';
+import NewFood from './components/NewFood/NewFood';
+import NewDrinks from './components/NewDrinks/NewDrinks';
 
-export default function App() {
-  return (
-    <LandingPage />
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <NativeRouter>
+        <Route exact path='/' component={LandingPage} />
+        <Route path='/NewFood' component={NewFood} />
+        <Route path='/NewDrinks' component={NewDrinks} />
+      </NativeRouter>
+    )
+  }
 }
--- components/LandingPage/LandingPage.jsx
+++ components/LandingPage/LandingPage.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
+import { Link } from 'react-router-native';
 
-function LandingPage() {
+const LandingPage = () => {
     return (
         <View style={styles.optionsContainer}>
-            <ImageBackground style={styles.foodContainer} source={require('/Users/mfuechec/Desktop/RecipeBookNative/RecipeBook/assets/foodBackground.jpg')}>
-            </ImageBackground>
-            <ImageBackground style={styles.drinksContainer} source={require('/Users/mfuechec/Desktop/RecipeBookNative/RecipeBook/assets/drinksBackground.jpg')}>
-            </ImageBackground>
+            <Link style={styles.foodContainer} to='/NewFood'>
+                <ImageBackground style={styles.foodImage} source={require('/Users/mfuechec/Desktop/RecipeBook/assets/foodBackground.jpg')}>
+                </ImageBackground>
+            </Link>
+            <Link style={styles.drinksContainer} to='NewDrinks'>
+                <ImageBackground style={styles.drinksImage} source={require('/Users/mfuechec/Desktop/RecipeBook/assets/drinksBackground.jpg')}>
+                </ImageBackground>
+            </Link>
         </View>
     )
 }
@@ -25,9 +30,17 @@ const styles = StyleSheet.create({
         flex: .5,
         justifyContent: 'center'
     },
+    foodImage: {
+        width: '100%',
+        height: '100%'
+    },
     drinksContainer: {
         width: '100%',
         flex: .5,
         justifyContent: 'center'
+    },
+    drinksImage: {
+        width: '100%',
+        height: '100%'
     }
 })
