3cfec9aacd9e3b6871e4723ac0a265907fd16f22
Mark Fuechec <fuechec.mark@gmail.com>
2020-02-07 18:00:31 -0600

    Adjusted NavBar spacing

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet, ShadowPropTypesIOS } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Link } from 'react-router-native';
 
 const NavBar = (props) => {
     return (
         <View style={styles.NavBar} >
-            <Link to='/'>
-                <Text style={styles.homeButton}>
-                    Home
-                </Text>
-            </Link>
+            <View style={styles.upperBorder}></View>
+            <View>
+                <Link style={styles.homeButton} to='/'>
+                    <Text style={styles.text}>
+                        Home
+                    </Text>
+                </Link>
+            </View>
         </View>
     )
 }
@@ -19,13 +22,27 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '5%',
         backgroundColor: '#333333',
-        display: 'flex',
-        justifyContent: 'center'
+        display: 'flex'
+    },
+    upperBorder: {
+        height: '26%',
+        width: '100%',
+        backgroundColor: '#333333',
+        marginBottom: '.5%'
     },
     homeButton: {
         marginLeft: '3%',
         backgroundColor: 'white',
-        width: '2%'
+        width: '6%',
+        height: '74%',
+        borderRadius: 50,
+        justifyContent: 'center',
+        borderColor: 'red',
+        borderWidth: 1,
+        textAlign: 'center'
+    },
+    text: {
+        textAlign: 'center'
     }
 })
 
