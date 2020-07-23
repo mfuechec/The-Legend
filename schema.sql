70f09acf90617f5082d2f037c5a49ab9f8e1f3b8
Mark Fuechec <fuechec.mark@gmail.com>
2020-07-23 15:33:32 -0500

    Built schema with pointOfInterest and Trending table

--- schema.sql
+++ schema.sql
@@ -14,8 +14,15 @@ CREATE TABLE PointOfInterest (
     id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     image VARCHAR(255) NOT NULL,
-    coordinates point NOT NULL,
+    latitude DECIMAL NOT NULL,
+    longitude DECIMAL NOT NULL,
     averageRating int NOT NULL
 );
 
--- INSERT INTO PointOfInterest (name, image, coordinates, averageRating) VALUES ('Austin Capitol Building', );
+INSERT INTO PointOfInterest (name, image, latitude, longitude, averageRating) VALUES ('Austin Capitol Building', 's3://the-legend/Austin State Capitol/0.jpg', 30.274399, -97.740492, 3);
+INSERT INTO PointOfInterest (name, image, latitude, longitude, averageRating) VALUES ('Barton Springs', 's3://the-legend/Barton Springs/0.jpeg', 30.259110, -97.752220, 4.5);
+INSERT INTO PointOfInterest (name, image, latitude, longitude, averageRating) VALUES ('Sixth Street', 's3://the-legend/Sixth Street/0.jpeg', 30.266746, -97.738058, 1.2);
+
+INSERT INTO Trending (pointId) VALUES (1);
+INSERT INTO Trending (pointId) VALUES (2);
+INSERT INTO Trending (pointId) VALUES (3);
